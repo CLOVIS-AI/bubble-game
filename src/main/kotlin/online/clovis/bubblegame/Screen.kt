6508cae1673d0ce20c45a6d274722da44efc224c
Ivan Canet <ivan.canet@gmail.com>
2018-10-18 16:53:36 +0200

    Added the Screen

---
+++ src/main/kotlin/online/clovis/bubblegame/Screen.kt
@@ -0,0 +1,20 @@
+package online.clovis.bubblegame
+
+import online.clovis.bubblegame.Game.players
+import processing.core.PApplet
+
+object Screen : PApplet() {
+
+    override fun settings() {
+        size(500, 500)
+    }
+
+    override fun setup() {
+        background(0)
+    }
+
+    override fun draw() {
+        
+    }
+
+}
