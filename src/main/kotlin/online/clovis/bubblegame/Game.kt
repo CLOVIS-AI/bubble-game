5d64a2c2c1f021e89e91538ac53238c95cadda3d
Ivan Canet <ivan.canet@gmail.com>
2018-10-18 18:36:12 +0200

    Update, draw Obstacles, remove them when they are uneeded

--- src/main/kotlin/online/clovis/bubblegame/Game.kt
+++ src/main/kotlin/online/clovis/bubblegame/Game.kt
@@ -1,16 +1,38 @@
 package online.clovis.bubblegame
 
+import online.clovis.bubblegame.obstacles.Obstacle
+import online.clovis.bubblegame.obstacles.Platform
+
 class Game(val players: Set<Player>) {
 
     var ground = Screen.height.toFloat()
         private set
 
-    fun draw(){
-        Screen.background(0)
-        players.forEach {
-            it.move(this)
-            it.draw()
+    private var obstacles = ArrayList<Obstacle>()
+
+    fun update(){
+        if(Screen.random(10f) < 1)
+            obstacles.add(Platform(
+                    Screen.random(Screen.width.toFloat()),
+                    50f
+                )
+            )
+
+        obstacles.removeIf { !it.isOnScreen() }
+        obstacles.forEach { it.move() }
+
+        players.forEach { player ->
+            player.move(this)
+            obstacles.forEach { it collideWith player }
         }
+    }
+
+    fun draw(){
+        update()
+
+        Screen.background(0)
+        obstacles.forEach { it.draw() }
+        players.forEach { it.draw() }
     }
 
 }
