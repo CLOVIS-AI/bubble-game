ebc68054541d343d797f3be7d07bb0c1ca4c5712
Ivan Canet <ivan.canet@gmail.com>
2018-10-18 16:52:45 +0200

    Created Main method

---
+++ src/main/kotlin/online/clovis/bubblegame/Main.kt
@@ -0,0 +1,8 @@
+package online.clovis.bubblegame
+
+import processing.core.PApplet
+
+fun main(args: Array<String>){
+    val main = Screen
+    PApplet.runSketch(arrayOf("clovis.online.bubblegame.Screen"), main)
+}
