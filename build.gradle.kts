aa1b81fed41ea7b0d220c74648247154ab008675
Ivan Canet <ivan.canet@gmail.com>
2018-10-18 16:51:54 +0200

    Added Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.2.71"
+}
+
+group = "online.clovis"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+
+    compile(group = "org.processing", name = "core", version = "3.3.7")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
